71419eadb89388a08997cdb0965c2a2d2ed59fe8
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-22 15:36:38 +0300

    update

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,8 +1,29 @@
 import { FC } from 'react'
+import Hero from '../../components/mail-page/hero'
+import Buttons from '../../components/mail-page/buttons'
+import Filter from '../../components/mail-page/filter'
+import List from '../../components/mail-page/list'
 
 const Main: FC = () => {
     return (
-        <div>Main</div>
+        <div>
+            <Hero />
+
+            <Buttons />
+
+            <h1 className='text-[20px] md:text-[26px] lg:text-[30px] xl:text-[36px] font-semibold mb-4 md:mt-[28px] xl:mt-[32px]'>Sana Uygun Seçenekler</h1>
+
+            <div className='grid grid-cols-4 gap-4'>
+                <div className='max-ld:hidden'>
+                    <Filter />
+                </div>
+                <div className='cols-span-4 lg:col-span-3'>
+                    <List />
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
