8daff3c998ab0025b7acc4b2463e67476441e330
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-26 17:37:47 +0300

    update

--- src/components/detail-page/size.tsx
+++ src/components/detail-page/size.tsx
@@ -1,35 +1,36 @@
 import { FC, useState } from "react";
 import { Shoe } from "../../types";
-import { numbers } from "../../utils/constants";
+import { colors } from "../../utils/constants";
 
 interface Props {
     item: Shoe;
 }
 
-const Size: FC<Props> = ({ item }) => {
+const Color: FC<Props> = ({ item }) => {
     const [selected, setSelected] = useState<string>("");
 
-    const toggle = (size: string) => {
-        setSelected(selected === size ? "" : size);
+    const toggle = (id: string) => {
+        setSelected(selected === id ? "" : id);
     };
 
     return (
         <div>
-            <h2 className="mb-4 font-semibold">Numara Seçiniz</h2>
-            <div className="grid grid-cols-5 gap-4">
-                {numbers.map((num) => {
-                    const inStock = item.size.split(",").includes(num);
-                    const found = selected === num;
+            <h2 className="font-semibold mb-3">Renk Seçiniz</h2>
+
+            <div className="flex gap-5">
+                {item.color.split(",").map((id) => {
+                    const color = colors.find((i) => i.id === id);
+
+                    const isSelected = selected === id;
+
                     return (
-                        <button
-                            onClick={() => toggle(num)}
-                            disabled={!inStock}
-                            type="button"
-                            className={`py-2 px-4 lg:px-0 text-center rounded-md cursor-pointer transition hover:bg-zinc-400
-                                 disabled:bg-[#D2D1D3] disabled:text-[#8F8C91] ${found ? "bg-black text-white" : "bg-white"
-                                } `}>
-                            {num}
-                        </button>
+                        <div className={isSelected ? "ring-3 ring-my-blue rounded-full" : ""}>
+                            <div
+                                onClick={() => toggle(id)}
+                                className="m-1 size-9 rounded-full cursor-pointer"
+                                style={{ background: color?.code }}
+                            />
+                        </div>
                     );
                 })}
             </div>
@@ -37,4 +38,4 @@ const Size: FC<Props> = ({ item }) => {
     );
 };
 
-export default Size;
+export default Color;
--- src/hooks/useShoes.ts
+++ src/hooks/useShoes.ts
@@ -1,44 +1,45 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { shoesApi } from "../services/api";
 import { ShoeData } from "../types";
 
-
 export function useShoes() {
+    const queryClient = useQueryClient();
 
     const shoes = useQuery({
-        queryKey: ['shoes'],
+        queryKey: ["shoes"],
         queryFn: () => shoesApi.getAll().then((res) => res.data),
-    })
+    });
 
-    const shoe = (id: string) => useQuery({
-        queryKey: ['shoe', id],
-        queryFn: () => shoesApi.getById(id).then((res) => res.data)
-    })
+    const shoe = (id: string) =>
+        useQuery({
+            queryKey: ["shoe", id],
+            queryFn: () => shoesApi.getById(id).then((res) => res.data),
+        });
 
-    const create = (data: ShoeData) =>
-        useMutation({
-            mutationFn: () => shoesApi.create(data),
-            onSuccess: () => {
-                alert('Oluşturuldu');
-            }
-        })
+    const create = useMutation({
+        mutationFn: (data: ShoeData) => shoesApi.create(data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["shoes"] });
+            alert("Oluşturuldu");
+        },
+    });
 
-    const edit = (id: string, data: ShoeData) =>
-        useMutation({
-            mutationFn: () => shoesApi.edit(id, data),
-            onSuccess: () => {
-                alert('Düzenlendi');
-            }
-        })
+    const edit = useMutation({
+        mutationFn: ({ id, data }: { id: string; data: ShoeData }) =>
+            shoesApi.edit(id, data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["shoes"] });
+            alert("Düzenlendi");
+        },
+    });
 
-    const remove = (id: string) =>
-        useMutation({
-            mutationFn: () => shoesApi.delete(id),
-            onSuccess: () => {
-                alert('Kaldırıldı');
-            }
-        })
+    const remove = useMutation({
+        mutationFn: (id: string) => shoesApi.delete(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["shoes"] });
+            alert("Kaldırıldı");
+        },
+    });
 
-
-    return { shoes, shoe, create, edit, remove }
+    return { shoes, shoe, create, edit, remove };
 }
